86c5e7623bd2f390224b0c5337958a501f94e5ff
unknown <adilmnr27@gmail.com>
2020-07-21 23:47:15 +0530

    UseEffect to prepolutate the form the basis of props.match .params

--- src/components/ManageCourses.js
+++ src/components/ManageCourses.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect} from 'react'
 import CourseForm from './CourseForm'
 import * as courseApi from "../api/courseApi";
 import { toast } from 'react-toastify'
@@ -11,6 +11,19 @@ export default function ManageCourses(props) {
         authorId: null,
         category: ""
     })
+
+    /* when the slug changes , rerun it.
+    Because if any dependecy in the below array changes, useEffect reruns
+    */
+    useEffect(() => {
+        if(props.match.params.slug){
+            courseApi.getCourseBySlug(props.match.params.slug).then(_course=>{
+                setCourse(_course);
+            })
+        }
+    }, [props.match.params.slug])
+
+
 
     /*
     ...course indicates that a copy of the object is created.
